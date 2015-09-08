79511cb2bd10ce3cbc4c4012a7f459ea0b662513
ullambana0715 <ullambana0715@gmail.com>
2015-09-08 23:43:59 +0800

    use bin files to test insufficient space

--- gen/com/android/automation/R.java
+++ gen/com/android/automation/R.java
@@ -25,33 +25,42 @@ public final class R {
         public static final int ic_alert=0x7f020001;
         public static final int ic_launcher=0x7f020002;
         public static final int itembg=0x7f020003;
-        public static final int start_server_bg_click=0x7f020004;
-        public static final int start_server_bg_normal=0x7f020005;
-        public static final int textbg=0x7f020006;
+        public static final int mainbg=0x7f020004;
+        public static final int off=0x7f020005;
+        public static final int on=0x7f020006;
+        public static final int pagerbg=0x7f020007;
+        public static final int start_server_bg_click=0x7f020008;
+        public static final int start_server_bg_normal=0x7f020009;
+        public static final int textbg=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000f;
-        public static final int bottombar=0x7f08000c;
-        public static final int chart1=0x7f080001;
-        public static final int chart2=0x7f080002;
-        public static final int chart3=0x7f080003;
-        public static final int cutacverage=0x7f080008;
-        public static final int cuttimes=0x7f080007;
-        public static final int endserver=0x7f08000e;
-        public static final int grid=0x7f08000b;
-        public static final int gridbackgroud=0x7f080009;
-        public static final int items=0x7f080004;
-        public static final int machineno=0x7f080005;
-        public static final int runningstatus=0x7f080006;
-        public static final int startserver=0x7f08000d;
-        public static final int totalmachine=0x7f08000a;
+        public static final int action_settings=0x7f080011;
+        public static final int bottombar=0x7f08000b;
+        public static final int button1=0x7f080010;
+        public static final int chart=0x7f080002;
+        public static final int chartlist=0x7f080001;
+        public static final int cutacverage=0x7f080007;
+        public static final int cuttimes=0x7f080006;
+        public static final int editText1=0x7f08000f;
+        public static final int endserver=0x7f08000d;
+        public static final int grid=0x7f08000a;
+        public static final int gridbackgroud=0x7f080008;
+        public static final int items=0x7f080003;
+        public static final int machineno=0x7f080004;
+        public static final int runningstatus=0x7f080005;
+        public static final int startserver=0x7f08000c;
+        public static final int textView1=0x7f08000e;
+        public static final int totalmachine=0x7f080009;
         public static final int vPager=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int chart=0x7f030001;
-        public static final int items=0x7f030002;
-        public static final int mainframe=0x7f030003;
+        public static final int chartitems=0x7f030002;
+        public static final int items=0x7f030003;
+        public static final int mainframe=0x7f030004;
+        public static final int piechart=0x7f030005;
+        public static final int sendstop=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -60,6 +69,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int machine_no=0x7f050004;
         public static final int totalmachines=0x7f050003;
     }
     public static final class style {
